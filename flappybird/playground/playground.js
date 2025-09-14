e36733805ecc0720327c56772b9642a791ff2625
potato023 <adrianbon2012@gmail.com>
2025-09-14 08:16:08 +0000

    Sep 14, 2025, 8:16 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -1,3 +1,19 @@
+//1)create two main functions
+//2)set up canvas (800,400)
+//3)declare a var square and create a  sqaure sprite (X,Y,Width,Hegiht)
+//x = midpoint of canvas y midpoint of canvas
+//4)declare a var circle and create a circle sprite (X,Y,D)
+//x= 100, y = 350 , D of ur choosing
+//5)declare a var pipe and create a new group
+//setup gravity
+//7)while loop syntax and for loop syntax
+//6)left click create a dynamic circle add in bounciness and gravity
+//add in color red to ballsprite
+//8)create a sprite at mouse position 
+//9)declare var popimage and preload it in preload function
+//10)// --- Exercise: Age category classification --- 
+//if score 90 A ifscore70 b else c.
+
 function preload(){
 
 }
