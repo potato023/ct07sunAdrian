6b88f2f210f4b8c4b416c7d264a88e6beb124767
potato023 <adrianbon2012@gmail.com>
2025-09-14 09:11:08 +0000

    Sep 14, 2025, 9:11 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -14,7 +14,7 @@ function preload(){
   flapUpImg = loadImage('assets/yellowbird-upflap.png');
   flapDownImg = loadImage('assets/yellowbird-downflap.png');
   pipe = loadImage('assets/pipe-green.png');
-)
+
   flapsound =createAudio("Assets/sfx_wings.mp3");
 pointsound =createAudio("Assets/sfx_point.mp3");
 failsound =createAudio("Assets/sfx_die.mp3");
