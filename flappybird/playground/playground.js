b5ccfcc2c930e19f1fa0573d17852e5af7482398
potato023 <adrianbon2012@gmail.com>
2025-09-14 08:22:17 +0000

    Sep 14, 2025, 8:22 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -22,7 +22,7 @@ function setup(){
     
 }
 function draw(){
-    square=(400,200,10,10)
-    circle=(100,350,10)
+    square=(400,200,10,10);
+    circle=(100,350,10);
 }
-pipe = newg
+pipe = newgroup;
