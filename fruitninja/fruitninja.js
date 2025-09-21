74fba54c6edbd5e166af8b78d3481b5957b7549e
potato023 <adrianbon2012@gmail.com>
2025-09-21 09:03:27 +0000

    Sep 21, 2025, 9:03 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -10,7 +10,7 @@ function draw(){
     watermelon = new sprite();
     fruit = new group()
 
-    //use mouse. to slice fruits when clicked and over fruit
+  
 
 
 }
---
+++ fruitninja/fruitninja01.js
@@ -0,0 +1,16 @@
+let watermelon;
+let fruit;
+
+
+function setup(){
+    new Canvas(800,400);
+}
+
+function draw(){
+    watermelon = new sprite();
+    fruit = new group()
+
+  
+
+
+}
